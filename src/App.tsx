879cfab365a6a108670ee138f4b894856b937a0c
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2023-03-01 23:48:04 +0900

    created difficulty level entry window

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-
-
 function App() {
   return (
     <div className="App">
-      
+      <h1>Choose difficulty Level</h1>
+      <div className="level_container">
+        <span className="level"></span>
+        <span className="level"></span>
+        <span className="level"></span>
+      </div>
     </div>
   );
 }
